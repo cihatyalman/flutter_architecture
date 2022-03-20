d4266b91d017d00150349fc49fc74de166663b80
cihat <bmcihatyalman@gmail.com>
2022-03-20 18:56:56 +0300

    Update edited

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -6,46 +6,30 @@ class HomeScreen extends StatelessWidget {
   static const route = 'HomeScreen';
 
   late _ScreenWidgets screenWidgets;
-  final screenData = _ScreenData();
 
   @override
   Widget build(BuildContext context) {
     screenWidgets = _ScreenWidgets(context: context, widget: this);
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text(route)),
-      body: Column(
-        children: [
-          screenWidgets.body(route),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            screenWidgets.body(route),
+          ],
+        ),
       ),
     );
   }
 }
 
 class _ScreenWidgets {
-  final HomeScreen widget;
   final BuildContext context;
-  
+  final HomeScreen widget;
+
   _ScreenWidgets({required this.context, required this.widget});
 
   Widget body(String text) {
     return Center(child: Text(text));
-  }
-}
-
-class _ScreenData {
-  String? name;
-  int? age;
-
-  _ScreenData({
-    this.name,
-    this.age,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'age': age,
-    };
   }
 }
